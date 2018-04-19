6af108aef7a62166f7aa91c04646aa2d25c2b274
ChuckEng <crystal_bit@hotmail.com>
2018-04-19 13:30:41 -0700

    Add auto-complete for jmp names.

--- jmp.sh
+++ jmp.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
-
+ 
 JMPSCRIPT="${HOME}/bin/jmp.py"
-
+ 
 function jmp() {
     if [[ ${1} == "-"* ]]; then
         python3 ${JMPSCRIPT} ${@}
         return
     fi
-
+ 
     output="$(python3 ${JMPSCRIPT} ${@})"
     if [[ -z "${output}" ]]; then
         if [[ -n "${1}" ]]; then
@@ -27,3 +27,19 @@ function jmp() {
         false
     fi
 }
+ 
+# this provide tab autocompletion for your jump keys. Tested in bash and zsh
+_jmp()
+{
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    local keys=`awk '{print $1}' ~/.jump_list.txt | tr '[:]' ' ' | grep "^$cur" | tr '\n' ' '`
+ 
+    if [[ ${cur} == * ]] ; then
+        COMPREPLY=( $(compgen -W "${keys}" -- ${cur_word}) )
+    else
+        COMPREPLY=()
+    fi
+    return 0
+}
+ 
+complete -F _jmp jmp
